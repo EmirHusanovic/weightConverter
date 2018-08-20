47eeb4f876c1774dcde6dee87175bd815ae4921e
EmirHusanovic <emirhusanovic@outlook.com>
2018-08-20 11:38:45 +0200

    Added

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+	
+
+	document.getElementById("output").style.visibility = "hidden";
+document.getElementById("lbsInput").addEventListener("input", function(e) {
+  document.getElementById("output").style.visibility = "visible";
+  let lbs = e.target.value;
+  console.log(lbs);
+  document.getElementById("gramsOutput").innerHTML = lbs / 0.0022046;
+  document.getElementById("kgOutput").innerHTML = lbs / 2.2046;
+  document.getElementById("ouncesOutput").innerHTML = lbs * 16;
+});
+
+
+</script>
